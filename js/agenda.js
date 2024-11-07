4c7b87b1097cc6f244db7c4ce36be058bb4203a7
uo287577 <uo287577@uniovi.es>
2024-11-07 17:56:04 +0100

    Add files via upload

---
+++ js/agenda.js
@@ -0,0 +1,10 @@
+/* Miguel Fernández Huerta UO287577 */
+
+"use strict"; // para que se haga comprobación de tipos en tiempo de ejecución
+class Agenda
+{
+    constructor()
+    {
+        this.url = "https://ergast.com/api/f1/current";
+    }
+}
